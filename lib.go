64aefe48b53322fae9c6d3e2c8ab7c4d44a168f5
Paulo Simão <paulo@digitalcircle.com.br>
2022-03-07 07:39:19 -0300

    add subbuildinfo

--- lib.go
+++ lib.go
@@ -1,6 +1,9 @@
 package buildinfo
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 var Ver string
 var BuildDate string
@@ -9,10 +12,25 @@ var BuildUser string
 var Product string
 
 func String() string {
+
+	extraInfo := ""
+	for _, v := range []string{"buildinfo.yaml", "buildinfo.json", "buildinfo.txt", "buildinfo"} {
+		_, err := os.Stat(v)
+		if err != nil {
+			bs, err := os.ReadFile(v)
+			if err != nil {
+				extraInfo = string(bs)
+				break
+			}
+		}
+	}
+
 	return fmt.Sprintf(
 		`%s: 
   - Ver: %s
   - Built at: %s
   - Built in Host: %s
-  - Built by: %s`, Product, Ver, BuildDate, BuildHost, BuildUser)
+  - Built by: %s
+  %s`, Product, Ver, BuildDate, BuildHost, BuildUser, extraInfo)
+
 }
